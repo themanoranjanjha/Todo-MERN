7f62f3654aa885442b62bc923c9e2d6a025e724d
Manoranjan1k <manasj790@gmail.com>
2025-01-08 21:00:49 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  
+  return (
+    <>
+      <h1 className=" w-full h-screen text-center bg-black text-white text-3xl hover:bg-slate-600">hey this is todo App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/Readme.md
@@ -0,0 +1,1 @@
+# A backend of Todo App.
