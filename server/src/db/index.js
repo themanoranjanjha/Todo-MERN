bfd6018bc94eacb2b2a6b6b3b0f73d4659c5e37e
Manoranjan1k <manasj790@gmail.com>
2025-01-10 15:30:27 +0530

    add database connection setup and update package.json scripts

---
+++ server/src/db/index.js
@@ -0,0 +1,18 @@
+import { DB_NAME } from '../constants.js';
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+      const connectionInstance = await mongoose.connect
+      (`${process.env.MONGO_URI}/${DB_NAME}`)
+      console.log(`\n MongoDB connected !! DB HOST: 
+        ${connectionInstance.connection.host} `);
+
+    } catch (error) {
+        console.log('Error while connecting to DB', error);
+        process.exit(1);
+        
+    }
+}
+
+export default connectDB; 
