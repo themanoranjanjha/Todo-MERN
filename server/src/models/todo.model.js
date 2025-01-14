9c14bef7e11979907476d1e69c837869bef221e1
Manoranjan1k <manasj790@gmail.com>
2025-01-14 22:18:36 +0530

    add user and todo models with bcrypt and jwt integration; update package.json dependencies

---
+++ server/src/models/todo.model.js
@@ -0,0 +1,26 @@
+import mongoose, {Schema} from 'mongoose';
+
+const todoSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export const Todo = mongoose.model('Todo', todoSchema);
